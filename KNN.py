cd54131a7c5a4491cdc52e9e63ce9fb61ea2e625
faishalrachman <faishalr97@gmail.com>
2017-11-14 21:03:52 +0700

    Pusing :(

--- KNN.py
+++ KNN.py
@@ -1,100 +1,83 @@
-
 # COPAS QUICK SORT
 def quickSort(alist):
-   quickSortHelper(alist,0,len(alist)-1)
-
-def quickSortHelper(alist,first,last):
-   if first<last:
-
-       splitpoint = partition(alist,first,last)
-
-       quickSortHelper(alist,first,splitpoint-1)
-       quickSortHelper(alist,splitpoint+1,last)
+    quickSortHelper(alist, 0, len(alist) - 1)
 
 
-def partition(alist,first,last):
-   pivotvalue = alist[first][0]
+def quickSortHelper(alist, first, last):
+    if first < last:
+        splitpoint = partition(alist, first, last)
 
-   leftmark = first+1
-   rightmark = last
-
-   done = False
-   while not done:
-
-       while leftmark <= rightmark and alist[leftmark][0] <= pivotvalue:
-           leftmark = leftmark + 1
-
-       while alist[rightmark][0] >= pivotvalue and rightmark >= leftmark:
-           rightmark = rightmark -1
-
-       if rightmark < leftmark:
-           done = True
-       else:
-           temp = alist[leftmark]
-           alist[leftmark] = alist[rightmark]
-           alist[rightmark] = temp
-
-   temp = alist[first]
-   alist[first] = alist[rightmark]
-   alist[rightmark] = temp
+        quickSortHelper(alist, first, splitpoint - 1)
+        quickSortHelper(alist, splitpoint + 1, last)
 
 
-   return rightmark
+def partition(alist, first, last):
+    pivotvalue = alist[first][0]
+
+    leftmark = first + 1
+    rightmark = last
+
+    done = False
+    while not done:
+
+        while leftmark <= rightmark and alist[leftmark][0] <= pivotvalue:
+            leftmark = leftmark + 1
+
+        while alist[rightmark][0] >= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark - 1
+
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = temp
+
+    temp = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = temp
+
+    return rightmark
 
 
-
-#NGODINGNYA DARI SINI
+# NGODINGNYA DARI SINI
 
 import math as m
-f = open("ganteng.csv","r")
+
+f = open("ganteng.csv", "r")
 a = f.readlines()
 
-# FOLD ARRAY
-def foldarray(arr,testong):
-    arraytraining = []
-    arraytesting = []
-    for i in range(len(arr)):
-        s = arr[i].split(',')
-        if (int(s[4]) == testong):
-            arraytesting.append(arr[i])
-        elif int(s[4]) == 4:
-            pass
-        else:
-            arraytraining.append(arr[i])
-    return [arraytraining,arraytesting]
 
-folded = foldarray(a,4)
-
-
-def euclidean(x1,y1,x2,y2):
-    deltax = abs(float(x2)-float(x1))
+def euclidean(x1, y1, x2, y2):
+    deltax = abs(float(x2) - float(x1))
     # print deltax
     # print deltax
-    deltay = abs(y2-y1)
+    deltay = abs(y2 - y1)
     deltax = float(deltax) ** 2
     deltay = float(deltay) ** 2
-    return m.sqrt(deltax+deltay)
+    return m.sqrt(deltax + deltay)
 
 
-
-def euclidab(x1,y1,source,a,b):
+def euclidab(x1, y1, source, a, b):
     x = []
-    for i in range(a,b):
+    for i in range(a, b):
         s = source[i].split(',')
         x2 = float(s[1])
         y2 = float(s[2])
-        nilai = euclidean(x1,y1,x2,y2)
-        x.append([nilai,s[3].replace('\n','').replace('\r',''),i+1])
-    # print i
+        nilai = euclidean(x1, y1, x2, y2)
+        x.append([nilai, s[3].replace('\n', '').replace('\r', ''), i + 1])
+    # print "Nilai X sebelum di sorting : ", x
     quickSort(x)
+    # print "Nilai X sesudah di sorting : ", x
     return x
 
-def getresultinK(arr,K):
+
+def getresultinK(arr, K):
     ya = 0
     tidak = 0
-    for i in range(0,K):
+    for i in range(0, K):
         decision = arr[i][1]
-        if (decision == "Ya") :
+        if (decision == "Ya"):
             ya = ya + 1
         else:
             tidak = tidak + 1
@@ -105,43 +88,45 @@ def getresultinK(arr,K):
         return "Tidak"
 
 
+def hitungArray(x, y, z, K, a):
+    # hasil = []
+    evaluasi = []
+    for i in range(x, y):
+        evaluasi.append(a[i])
+    training = []
+    for i in range(0, x):
+        training.append(a[i])
+    for i in range(y, z):
+        training.append(a[i])
 
+    for i in range(len(evaluasi)):
+        s = evaluasi[i].split(',')
+        x1 = float(s[1])
+        y1 = float(s[2])
+        hasileuclidean = euclidab(x1, y1, training, 0, len(training))
+        # print hasileuclidean
+        # hasil.append(getresultinK(hasileuclidean, K))
+        print getresultinK(hasileuclidean,K)#, hasileuclidean.__getslice__(0,K)
+    # return hasil
 
-
-testing = []
-for i in range(0,5):
-	testing.append(a[i])
-
-evaluasi = []
-for i in range(5,15):
-	evaluasi.append(a[i])
-
-for i in range(len(testing)):
-	s = testing[i].split(',')
-	x1 = float(s[1])
-	y1 = float(s[2])
-	hasileuclidean = euclidab(x1, y1, evaluasi, 0, len(evaluasi))
-	print getresultinK(hasileuclidean, 5)
-	print hasileuclidean
-
-
-
-
-def foldarray(arr,K):
-    panjang = len(arr)
-    s = m.floor(panjang/K)
-
-    
-
+# def gantengArray(array,panjangarray,kfold,knn):
+#     upil = []
+#     awal = 0
+#     panjangkfold = len(array)/kfold
+#     for i in range(0,kfold-1):
+#         upil.extend(hitungArray(awal,awal+panjangkfold,panjangarray,knn,array))
+#         print awal, awal+panjangkfold-1,panjangarray,knn,array
+#         awal = awal+panjangkfold-1
+#     return upil
 #
-# s = a[0].split(',')
-# x1 = float(s[1])
-# y1 = float(s[2])
-# s = a[5].split(',')
-# x2 = float(s[1])
-# y2 = float(s[2])
-# print x1,y1,x2,y2
-# print euclidean(x1,y1,x2,y2)
-# print x1, x2, y1, y2
-# print euclidean(x1,x2,y1,y2)
+# hitung = gantengArray(a,15,5,1)
+# for i in range(0,15):
+#     print hitung[i]
 
+# hitungArray(0,3,15,5,a)
+# hitungArray(3,6,15,5,a)
+# hitungArray(6,9,15,5,a)
+# hitungArray(9,12,15,5,a)
+# hitungArray(12,15,15,5,a)
+
+hitungArray(15,20,15,7,a)
